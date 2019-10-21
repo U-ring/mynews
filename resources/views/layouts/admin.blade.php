9c6795be8b2721aaa37a4490bf2f78db874962a5
”KANEKO <ukowtimety.eringi184@gmail.com>
2019-10-21 09:41:39 +0000

    PHP12課題です

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -22,6 +22,8 @@
    
     <!-- Styles -->
     {{-- Laravel標準で用意されているCSSを読み込みます--}}
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    {{-- この章の後半で作成するCSSを読み込みます --}}
     <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet" >
    </head>
    <body>
@@ -32,23 +34,44 @@
             <a class="navbar-brand" href="{{ url('/')}}">
               {{ config('app.name', 'Laravel')}}
             </a>
-            <button class="navbar-toggler" type="button" data-toggle
- ="collapse" data-target="#navbarSupportedContent" aria-controls
- ="navbarSupportedContent" aria-expanded="false" aria-label 
- ="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" 
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label ="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button> 
             
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
              
              <!-- Left Side Of Navbar -->
-             <ul class="navbar-nav mr-auto">
-               
-             </ul>
-             
-             <!-- Right Side Of Navbar -->
-             <ul class="navbar-nav ml-auto">
-             </ul>
+            <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+
+                        {{-- 以下を追記 --}}
+                        <!-- Authentication Links -->
+                        {{-- ログインしていなかったらログイン画面へのリンクを表示 --}}
+                        @guest
+                            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                        {{-- ログインしていたらユーザー名とログアウトボタンを表示 --}}
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                            @endguest
+                            {{-- 以上までを追記 --}}
+                        </ul>
            </div>
           </div>
         </nav>
