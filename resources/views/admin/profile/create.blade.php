81c2b61eeb39a8ec8c4380f6fe6baf2f25405a4d
”KANEKO <ukowtimety.eringi184@gmail.com>
2019-10-22 09:55:14 +0000

    PHP13課題です

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -7,6 +7,37 @@
         <div class="row">
             <div class="col-md-8 mx-auto">
                 <h2>My プロフィール</h2>
+                <form action="{{ action('Admin\ProfileController@create')
+                }}" method="post" enctype="multipart/form-data">
+                 <div class="form-group row">
+                    <label class="col-md-1">氏名</label>
+                    <div class="col-md-5">
+                        <input type="text" class="form-control" 
+                           name="yourname" value="{{ old('yourname')}}" >
+                 </div>
+                 <div class="form-group row">
+                   <label class><input type="checkbox" name="check">男性</label>
+                   <label class><input type="checkbox" name="check">女性</label>
+                 </div>      
+                 <div class="form-group row">
+                   <label class="col-md-2">趣味</label>
+                   <div class="col-md-10">
+                     <textarea class="form-control" 
+                     name="hobby" cols="50" rows="5"></textarea>
+                   </div>
+                 </div>
+                 <div class="form-group row">
+                     <label class="col-md-4">自己紹介</label>
+                     <div class="col-md-10">
+                     <textarea class="form-control" name="body" cols="60"rows="15">
+                       {{ old('body') }}</textarea>
+                     </div>   
+                 </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" 
+                    value="作成">
+                </form>
             </div>
         </div>
     </div>
